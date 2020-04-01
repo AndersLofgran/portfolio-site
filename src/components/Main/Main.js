c7bf72cd08650fe32a6f2a084563af34502be904
Anders Lofgran <anders.lofgran@gmail.com>
2020-04-01 14:37:51 -0600

    more navigation added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -15,32 +15,34 @@ const Main = () => {
   
   return (
     <div className="main" >
-      <img src={bkg}/>
+      <img src={bkg} alt='Background'/>
 
-      {page > 0 ?
-          <DoubleArrowRoundedIcon style={{transition: '1s', opacity: '1', transform: 'rotate(180deg)', filter: 'invert(1)', fontSize: '60px', margin: '15% 30px'}} onClick={() => pageChanger(-1)}/>
+      {page === 0 ?
+          <DoubleArrowRoundedIcon style={{zIndex: '2', position: 'fixed', top: '0', left: '20px', height: '100vh', transition: '1s', opacity: '0', transform: 'rotate(180deg)', fontSize: '60px'}}/>
         :
-          <DoubleArrowRoundedIcon style={{transition: '1s', opacity: '0', transform: 'rotate(180deg)', fontSize: '60px', margin: '15% 30px'}}/>
+          <DoubleArrowRoundedIcon style={{zIndex: '2', position: 'fixed', top: '0', left: '20px', height: '100vh', transition: '1s', opacity: '1', transform: 'rotate(180deg)', filter: 'invert(1)', fontSize: '60px'}} onClick={() => pageChanger(-1)}/>
       }
 
-      <div className='landing'>
-        <div>
-          <h1>Full</h1>
-          <h1>Stack</h1>
-          <h1>Web</h1>
-          <h1>Developer</h1>
+      <div className='main-content' style={{transform: `translateX(calc(${-page} * 100vw))`, transition: '1s'}}>
+        <div className='page'>
+          <div>
+            <h3>I'm a</h3>
+            <h1>Full</h1>
+            <h1>Stack</h1>
+            <h1>Web</h1>
+            <h1>Developer</h1>
+          </div>
         </div>
-        <p>Go on, take a peek.</p>
+        <div className='page'> <Portfolio /> </div>
+        <div className='page'> <About /> </div>
+        <div className='page'> <Contact /> </div>
       </div>
 
-      <Portfolio />
-      <About />
-      <Contact />
 
-      {page > 3 ?
-          <DoubleArrowRoundedIcon style={{transition: '1s', opacity: '0', fontSize: '60px', margin: '15% 30px'}}/>
+      {page === 3 ?
+          <DoubleArrowRoundedIcon style={{zIndex: '2', position: 'fixed', top: '0', right: '20px', height: '100vh', transition: '1s', opacity: '0', fontSize: '60px'}}/>
         :
-          <DoubleArrowRoundedIcon style={{transition: '1s', opacity: '1', filter: 'invert(1)', fontSize: '60px', margin: '15% 30px'}} onClick={() => pageChanger(1)}/>
+          <DoubleArrowRoundedIcon style={{zIndex: '2', position: 'fixed', top: '0', right: '20px', height: '100vh', transition: '1s', opacity: '1', filter: 'invert(1)', fontSize: '60px'}} onClick={() => pageChanger(1)}/>
       }
     </div>
   )
