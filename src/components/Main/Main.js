5009fd22294870a68125a96e9c9f6b457fa61992
Anders Lofgran <anders.lofgran@gmail.com>
2020-03-31 18:25:54 -0600

    landing page styling

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import bkg from '../../assets/bkg_overlay.png'
+import DoubleArrowRoundedIcon from '@material-ui/icons/DoubleArrowRounded'
 import './Main.css'
 
 export default function Main() {
   return (
     <div className="main" >
       <img src={bkg}/>
+
+      <DoubleArrowRoundedIcon style={{transform: 'rotate(180deg)', filter: 'invert(1)', fontSize: '60px', margin: '15% 30px'}} />
       <div className='main-content' >
         <div>
           <h1>Full</h1>
@@ -15,6 +18,7 @@ export default function Main() {
         </div>
         <p>Go on, take a peek.</p>
       </div>
+      <DoubleArrowRoundedIcon style={{filter: 'invert(1)', fontSize: '60px', margin: '15% 30px'}} />
     </div>
   )
 }
