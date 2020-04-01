3a21301bf6406eba1ecc92073c9f5791dcbc3034
Anders Lofgran <anders.lofgran@gmail.com>
2020-04-01 12:10:39 -0600

    added arrow navigators

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import bkg from '../../assets/bkg_overlay.png'
 import DoubleArrowRoundedIcon from '@material-ui/icons/DoubleArrowRounded'
+import Portfolio from '../Portfolio/Portfolio'
+import About from '../About/About'
+import Contact from '../Contact/Contact'
 import './Main.css'
 
-export default function Main() {
+const Main = () => {
+  const [page, setPage] = useState(0)
+
+  const pageChanger = num => {
+    setPage(page + num)
+  }
+  
   return (
     <div className="main" >
       <img src={bkg}/>
 
-      <DoubleArrowRoundedIcon style={{transform: 'rotate(180deg)', filter: 'invert(1)', fontSize: '60px', margin: '15% 30px'}} />
-      <div className='main-content' >
+      {page > 0 ?
+          <DoubleArrowRoundedIcon style={{transition: '1s', opacity: '1', transform: 'rotate(180deg)', filter: 'invert(1)', fontSize: '60px', margin: '15% 30px'}} onClick={() => pageChanger(-1)}/>
+        :
+          <DoubleArrowRoundedIcon style={{transition: '1s', opacity: '0', transform: 'rotate(180deg)', fontSize: '60px', margin: '15% 30px'}}/>
+      }
+
+      <div className='landing'>
         <div>
           <h1>Full</h1>
           <h1>Stack</h1>
@@ -18,7 +32,18 @@ export default function Main() {
         </div>
         <p>Go on, take a peek.</p>
       </div>
-      <DoubleArrowRoundedIcon style={{filter: 'invert(1)', fontSize: '60px', margin: '15% 30px'}} />
+
+      <Portfolio />
+      <About />
+      <Contact />
+
+      {page > 3 ?
+          <DoubleArrowRoundedIcon style={{transition: '1s', opacity: '0', fontSize: '60px', margin: '15% 30px'}}/>
+        :
+          <DoubleArrowRoundedIcon style={{transition: '1s', opacity: '1', filter: 'invert(1)', fontSize: '60px', margin: '15% 30px'}} onClick={() => pageChanger(1)}/>
+      }
     </div>
   )
 }
+
+export default Main
