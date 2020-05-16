7f16d1f963c250b2c490040fdec6d1121fb1d374
Anders Lofgran <anders.lofgran@gmail.com>
2020-05-15 17:31:07 -0700

    improved email link & about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import mug from '../../assets/mug.jpg'
 import './About.scss'
 
 export default function About() {
   return (
     <div className='about'>
-      <div>
-        <p>I am a Full Stack Web Developer drawn to simplicity and quality website design.</p>
-      </div>
+      <img src={mug} alt='' className='about-image'/>
+      <p>I am a Full Stack Web Developer drawn to simplicity and quality website design.</p>
     </div>
   )
 }
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,35 +1,28 @@
 /* eslint-disable no-lone-blocks */
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faLinkedin, faGithubSquare} from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
 import { faFileAlt, faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 import './Contact.scss'
 
 export default function Contact() {
+  const [showEmail, setShowEmail] = useState()
 
-{/* Email obfuscator script 2.1 by Tim Williams, University of Arizona
-Random encryption key feature coded by Andrew Moulden
-This code is freeware provided these four comment lines remain intact
-A wizard to generate this code is at http://www.jottings.com/obfuscator/ */}
-  let coded = "tD0Maj.Qs8NatD@NOtTQ.PsO"
-  let key = "Bmrvk3RfyD05Z4h2JPwCVj7QpOaebSg8KEHoiW9XLqF1A6GndxtMzuNcUIYsTl"
-  let shift = coded.length
-  let link = ""
-  for (let i=0; i<coded.length; i++) {
-    if (key.indexOf(coded.charAt(i)) === -1) {
-      let ltr = coded.charAt(i)
-      link += (ltr)
-    }
-    else {     
-      let ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
-      link += (key.charAt(ltr))
-    }
+  const handleShowEmail = () => {
+    return showEmail === undefined ? setShowEmail(1) : setShowEmail(undefined)
   }
   
   return (
     <div className='contact-page'>
-      <a  href={`mailto:${link}`} target='_blank noopener noreferrer' className='contact-icon-container'>
-        <FontAwesomeIcon icon={faPaperPlane} className='icon'/>
+      <a className='contact-icon-container' onClick={() => handleShowEmail()}>
+        {showEmail ?
+            <div className='show-email'>
+              <p>anders.lofgran@gmail.com</p>
+              {/* <button>Copy</button> */}
+            </div>
+          :
+            <FontAwesomeIcon icon={faPaperPlane} className='icon'/>
+        }
         <h2>Email me</h2>
       </a>
 
